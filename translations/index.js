fc4a7fb2430a784c8d74b904c725aed884f763c7
Jonathan Argentiero <jonathan.argentiero@gmail.com>
2017-02-16 10:24:57 +0100

    fix language codes

--- translations/index.js
+++ translations/index.js
@@ -1,23 +1,23 @@
 'use strict';
 
+const daDK = require('./da-DK');
 const deDE = require('./de-DE');
-const dkDK = require('./dk-DK');
 const enUS = require('./en-US');
+const enES = require('./es-ES');
 const frFR = require('./fr-FR');
 const itIT = require('./it-IT');
 const nlNL = require('./nl-NL');
 const seSE = require('./se-SE');
-const spSP = require('./sp-SP');
 
 var translations = {
+  "da-DK": daDK,
   "de-DE": deDE,
-  "dk-DK": dkDK,
   "en-US": enUS,
+  "en-ES": enES,
   "fr-FR": frFR,
   "it-IT": itIT,
   "nl-NL": nlNL,
   "se-SE": seSE,
-  "sp-SP": spSP,
 };
 
 module.exports = exports = translations;
