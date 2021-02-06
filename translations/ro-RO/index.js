a10f96d6f0c655b66c5fd31cbf3a836c4907710e
Download Information <komodoroot@protonmail.com>
2021-02-06 23:31:51 +0200

    Added translation for child-parent.json

---
+++ translations/ro-RO/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var childParentTranslation = require('./child-parent.json');
+var olderChildTranslation = require('./older-child.json');
+var teenAdultTranslation = require('./teen-adult.json');
+var youngChildTranslation = require('./young-child.json');
+
+var translation = {
+  "qst-young-child": youngChildTranslation,
+  "qst-older-child": olderChildTranslation,
+  "qst-teen-adult": teenAdultTranslation,
+  "qst-child-parent": childParentTranslation
+};
+
+module.exports = exports = translation;
